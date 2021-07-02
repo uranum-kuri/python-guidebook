c013f9c4f665028855cce6385aae05a53b2a8e1e
uranum-kuri <uranum.kuri@gmail.com>
2021-07-02 14:50:21 +0000

    add step 1 sample program

---
+++ step_01/sample_02.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2021 uranum
+
+print("Hello")  # 最初に実行されます
+print("World")  # 次に実行されます
---
+++ step_01/sample_05.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2021 uranum
+
+print("Hello\tWorld")  # タブの例です
+print("Hello\nWorld")  # 改行の例です
